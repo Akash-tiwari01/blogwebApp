4939cc18bd2bc1257f967a92834a2627826841c0
Akash Tiwari <akashtiwaari19bca014@gmail.com>
2024-12-30 00:44:02 +0530

    build(Register): module now new users can register into database
    some issues are there like no registration Authintication no cross check is user already exist or not duplicacy of user
    
    lack some functionality like customise profile currently username is email and no profile picture no bio no user description and long way to go we are still in

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const file = process.env.DATA_FILE
 
 const readData = ()=>{
     try{
-         return fs.readFileSync(file,'utf8')
+         return JSON.parse(fs.readFileSync(file,'utf-8'))
     }catch(err)
     {
         console.log(`Error in Reading Data ${err}`);
@@ -22,8 +22,18 @@ const readData = ()=>{
 }
 
 const writeData = (data)=>{
-    console.log(data);
+    
+    try{
+        fs.writeFileSync(file,JSON.stringify(data,null,2),"utf-8",)
+        return true;
+    }catch(err)
+    {
+        console.log(`Error in Writing Data ${err}`);
+        return false;
+    }
 }
+
+const authenticate = (user)=>true
 
 app.get('/',(req,res)=>{
     const user = {islogin:false}
@@ -40,10 +50,23 @@ app.get("/registration",(req,res)=>{
 
 app.post('/registration',(req,res)=>{
 
-    const user = readData()
-    const id = Date.now().toString
-    console.log(req.body);
-    res.send(req.body)
+    const users = readData()
+    const id = Date.now()
+    const {name,email,password,phoneNo} = req.body;
+    const isAuth = authenticate([name,email,password,phoneNo])
+    if(isAuth)
+    {
+        users[id]={id:id,name:name,email:email,password:password,phoneNo:phoneNo,isLogin:false};
+        const success = writeData(users);
+        if(success)
+        {
+            res.render("welcome",{user:users[id]});
+        }
+        else{
+            res.render("reregister")
+        }
+    }
+      
 })
 
 
