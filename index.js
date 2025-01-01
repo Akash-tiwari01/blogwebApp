647a2c573321fbd216fb5205e7b566be1a345058
Akash Tiwari <akashtiwaari19bca014@gmail.com>
2025-01-01 17:37:20 +0530

    Login functionality is added

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const readData = ()=>{
     }catch(err)
     {
         console.log(`Error in Reading Data ${err}`);
-        return {}
+        return []
     }
 }
 
@@ -33,7 +33,15 @@ const writeData = (data)=>{
     }
 }
 
-const authenticate = (user)=>true
+const authenticate = (user,users)=>{
+   const res = users.filter((element)=>(element.email===user.email || element.phoneNo == user.phoneNo));
+   if (res.length===0)
+   {
+    return true;
+   }
+   return false
+   
+}
 
 app.get('/',(req,res)=>{
     const user = {islogin:false}
@@ -53,20 +61,41 @@ app.post('/registration',(req,res)=>{
     const users = readData()
     const id = Date.now()
     const {name,email,password,phoneNo} = req.body;
-    const isAuth = authenticate([name,email,password,phoneNo])
+    const isAuth = authenticate({email:email,phoneNo:phoneNo},users)
     if(isAuth)
     {
-        users[id]={id:id,name:name,email:email,password:password,phoneNo:phoneNo,isLogin:false};
+        users.push({id:id,name:name,email:email,password:password,phoneNo:phoneNo,isLogin:false});
         const success = writeData(users);
         if(success)
         {
-            res.render("welcome",{user:users[id]});
+            res.render("welcome",{name:name});
         }
         else{
             res.render("reregister")
         }
     }
+    else{
+        res.send(`<h1>This User Already Exist<h1><br><a href="/login"><button>Login</button></a>`);
+    }
       
+})
+
+app.post('/login',(req,res)=>{
+    const {email,password} = req.body;
+    const user  = readData().filter((element)=>(element.email === email));
+    console.log(req.body);
+    console.log(user);
+
+    if(user[0].password === password && user.length)
+    {
+        
+        user[0].isLogin = true;
+        console.log(user);
+        res.render(`index`,{user:user[0]});
+    }
+    else
+    res.send('<script>alert("Wrong Email Id or Password");window.location.replace("/login");</script>')
+
 })
 
 
